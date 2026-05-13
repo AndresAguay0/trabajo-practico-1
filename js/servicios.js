a6cd41bddda440df3f918d3074fcec2915aafed8
Andres Aguayo <andyaguayo49@gmail.com>
2026-05-13 18:20:33 -0300

    Muestra de servicio en detalle implementada

--- js/servicios.js
+++ js/servicios.js
@@ -34,10 +34,67 @@ async function cargarServicios() {
                 <h3>$${servicio.precio}</h3>
                 <h3>${servicio.titulo}</h3>`
 
+            // Boton de mas info
+            const btnInfo = document.createElement("button")
+            btnInfo.textContent = "Mas Info"
+            btnInfo.classList.add("btn-info")
+
+            btnInfo.addEventListener("click", () => {
+                servicioPorId(servicio)
+            })
+
+            div.appendChild(btnInfo)
             cardContainer.append(div)
         })
     } catch (error) {
         console.log(`Error. No se pudieron traer los datos de los servicios. ${error}`)
+    }
+}
+
+async function servicioPorId(servicio) {
+
+    try {
+        if (!cardContainer) return
+
+        cardContainer.innerHTML = ""
+        cardContainer.innerHTML = `
+            <div class="loader-container">
+                <div class="loader"></div>
+                <p>Obteniendo los datos. Por favor espere...</p>
+            </div>`
+        
+        // Buscar datos en el back-end
+        console.log("- Buscando los datos del back-end -")
+        const response = await fetch(`http://localhost:3000/servicios/${servicio.id}`)
+        
+        // Guardar los datos como un json
+        const data = await response.json()
+
+        cardContainer.innerHTML = ""    // Eliminar el loader del html
+
+        console.log(data)
+
+        // CREO LOS DATOS PARA MOSTRARLOS
+        const div = document.createElement("div")
+            // MEJORAR EL TEMA DE LA IMAGEN <------------------------------------------------------------
+        div.innerHTML = `
+            <h3>${servicio.titulo}</h3>
+            <h3>$${servicio.precio}</h3>`
+
+        // Boton de mas info
+        const btnVolver = document.createElement("button")
+        btnVolver.textContent = "Volver"
+        btnVolver.classList.add("btn-volver")
+
+        btnVolver.addEventListener("click", () => {
+            cargarServicios()
+        })
+
+        div.appendChild(btnVolver)
+        cardContainer.append(div)
+
+    } catch (error) {
+
     }
 }
 
